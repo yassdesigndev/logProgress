99de459cca05e58f71cd5fe2b201ca351b564f37
yassdesigndev <yarass.work@gmail.com>
2025-03-20 17:24:26 +0000

    Deploy dist folder to gh-pages-fix5

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/logProgress/dist/sw.js', { scope: '/logProgress/dist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
